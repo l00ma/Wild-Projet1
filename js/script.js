33331cb8e549ab5cd6f1e5395a880cfc868a1ffc
l00ma <looma@protonmail.ch>
2023-03-21 21:03:45 +0100

    carousel et amelioration design

--- js/script.js
+++ js/script.js
@@ -27,7 +27,12 @@ function initMap(lat, long) {
 }
 //
 // end of openstreetmap code
+
 //// back button function used on top bar only
+window.onpageshow = function () {
+    window.scrollTo(0, 0);
+};
+
 function goBack() {
     window.history.back()
 }
@@ -60,3 +65,22 @@ const btnRight = document.getElementById("rightButton");
 btnRight.addEventListener("mouseenter", styleOnLeft);
 btnRight.addEventListener("mouseleave", styleOffLeft); */
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
